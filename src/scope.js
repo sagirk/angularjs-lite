9f37002477bc8c0b132142a979bba944dc828340
Sagir Khan <sagir.khan@gmail.com>
2017-09-10 09:35:01 +0530

    Keeping the digest going while it stays dirty

--- src/scope.js
+++ src/scope.js
@@ -17,17 +17,26 @@ Scope.prototype.$watch = function (watchFn, listenerFn) {
   this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$digestOnce = function () {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function (watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
       watcher.last = newValue;
       watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+      dirty = true;
     }
   });
+  return dirty;
+};
+
+Scope.prototype.$digest = function () {
+  var dirty;
+  do {
+    dirty = this.$digestOnce();
+  } while (dirty);
 };
 
 module.exports = Scope;
