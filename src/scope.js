3cccd1d175a7e43f1d4b45d472dc3819af7313cb
Sagir Khan <sagir.khan@gmail.com>
2017-09-11 07:28:04 +0530

    Value-based dirty checking

--- src/scope.js
+++ src/scope.js
@@ -9,25 +9,38 @@ function Scope() {
   this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function () { },
+    valueEq: !!valueEq,
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
   this.$$lastDirtyWatch = null;
 };
 
-Scope.prototype.$digestOnce = function () {
+Scope.prototype.$digest = function () {
+  var ttl = 10;
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw '10 digest iterations reached';
+    }
+  } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function () {
   var self = this;
   var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function (watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
-    if (newValue !== oldValue) {
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
       self.$$lastDirtyWatch = watcher;
-      watcher.last = newValue;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
       dirty = true;
     } else if (self.$$lastDirtyWatch === watcher) {
@@ -37,16 +50,12 @@ Scope.prototype.$digestOnce = function () {
   return dirty;
 };
 
-Scope.prototype.$digest = function () {
-  var ttl = 10;
-  var dirty;
-  this.$$lastDirtyWatch = null;
-  do {
-    dirty = this.$digestOnce();
-    if (dirty && !(ttl--)) {
-      throw '10 digest iterations reached';
-    }
-  } while (dirty);
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
 };
 
 module.exports = Scope;
