e9a8e54765aa37dd75e06b6c85f88e559d7bf844
Sagir Khan <sagir.khan@gmail.com>
2017-09-19 08:03:56 +0530

    $eval - Evalutating code in the context of a scope

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -373,4 +373,34 @@ describe('Scope', function () {
   
   });
 
+  describe('$eval', function () {
+
+    var scope;
+
+    beforeEach(function () {
+      scope = new Scope();
+    });
+
+    it('executes $evaled function and returns result', function () {
+      scope.aValue = 42;
+
+      var result = scope.$eval(function (scope) {
+        return scope.aValue;
+      });
+
+      expect(result).toBe(42);
+    });
+
+    it('passes the second $eval argument straight through', function () {
+      scope.aValue = 42;
+
+      var result = scope.$eval(function (scope, arg) {
+        return scope.aValue + arg;
+      }, 2);
+
+      expect(result).toBe(44);
+    });
+
+  });
+
 });
